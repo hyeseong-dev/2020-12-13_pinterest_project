75c5724ad752b6255767a3a4d58c070c5c39aeac
osori-magu <minjuda34@gmail.com>
2020-12-13 02:25:20 +0900

    UpdateView & UserCreationForm to update user profile

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
 
 app_name = 'accountapp'
 
@@ -13,5 +13,6 @@ urlpatterns = [
     # 별도로 로그아웃 템플릿을 만드는 것이 아닌 기존 템플릿을 이용할 것 이기에 template_name 지정 불필요
     path('create/', AccountCreateView.as_view(), name='create'), # url을 통해 해당 뷰가 호출되요.
     path('detail/<int:pk>/', AccountDetailView.as_view(), name='detail'),
+    path('update/<int:pk>/', AccountUpdateView.as_view(), name='update'),
 
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
+from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 
@@ -26,9 +27,7 @@ class AccountCreateView(CreateView): # 계정 생성을 위한 클래스
     model = User # 장고가 이미 만들어둔 User클래스를 사용함
     form_class = UserCreationForm# 장고가 제공한 기본적인 form을 이용(pycharm이면, UserCreationForm에 커서를 올리고 ctrl+b를 눌러 내부 소스코드를 확인해보세요.
     # form_class는 template에서 form이라는 기본 객체를 돌려줘 편리하게 우리가 지금 만드려는 input태그들을 미리 만들어 줘요.
-    print('정상적으로 찍히나요?11111')
     success_url = reverse_lazy('accountapp:hello_world') # reverse, reverse_lazy의 차이는 FBV와 CBV의 호출시 방식의 차이 때문임.
-    print('정상적으로 찍히나요?2222')
     template_name = 'accountapp/create.html' # 어떤 템플릿을 보여줄지 정합니다.
 
 
@@ -36,3 +35,11 @@ class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user' # 설정하지 않으면 기존 user context 이름으로는 모호하기 때문에 다른 사람의 페이지로 가도 자신의 정보만 보임
     template_name = 'accountapp/detail.html' # 내 프로필 html 템플릿 위치
+
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
