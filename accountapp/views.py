94ab192511ea80e40d615f8e4b935a7cd502e62c
osori-magu <minjuda34@gmail.com>
2020-12-12 19:40:20 +0900

    cache clear

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,20 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from accountapp.models import HelloWorld
+
 
 def hello_world(request):
     if request.method == 'POST':
 
-        return render(request, 'accountapp/helloworld.html', context={'text':'POST METHOD'})
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request, 'accountapp/helloworld.html', context={'text':'GET METHOD'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/helloworld.html', context={'hello_world_list':hello_world_list})
